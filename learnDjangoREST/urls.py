8c14705634412754ef6d39e64558f1c93c02f4e6
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-08-29 12:52:56 +0530

    Add urlpatterns in the snippets app

--- learnDjangoREST/urls.py
+++ learnDjangoREST/urls.py
@@ -25,6 +25,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('learnDjangoREST.snippets.urls')),
 ]
